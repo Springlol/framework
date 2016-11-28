8f7e87198ed95bc8e4c2d5b1cfc60bd63f3b5144
springlol <353256142@qq.com>
2016-11-28 21:13:38 +0800

    简单框架

--- src/main/java/com/zt/frame/filter/MyFilter.java
+++ src/main/java/com/zt/frame/filter/MyFilter.java
@@ -10,7 +10,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Iterator;
 import java.util.Map;
 
 /**
@@ -31,67 +33,81 @@ public class MyFilter implements Filter{
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		HttpServletRequest req = (HttpServletRequest)request;
 		HttpServletResponse resp = (HttpServletResponse)response;
-		//解析请求
-		if(!req.getRequestURI().endsWith(".action")){
+		//获取action
+		Action action = reqToAction(req);
+		if(action == null){
 			chain.doFilter(request,response);
 		}
-		//获取action name
-		String actionName = getRequestActionName(req);
-		Map<String, Object> context = ActionMapping.getContext();
-		Action action = (Action)context.get(actionName);
 		try {
-			//执行actionProxy
-			Object proxy = getProxy(action);
-			String resultCode = doAction(action,proxy, req);
-			Result result  = (Result)action.getResult().get(resultCode);
-			toLocation(result,proxy,req,resp);
+			//创建actionProxy
+			Object actionProxy = createActionProxy(action);
+			setPropertyToProxy(req,actionProxy);
+			String ret = actionExecute(actionProxy, action.getMethod());
+			Result result  = (Result)action.getResult().get(ret);
+			if (result != null) {
+				resultExecute(result,actionProxy,req,resp);
+			}
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-
-	private String getRequestActionName(HttpServletRequest request) {
-		String requestURI = request.getRequestURI();
-		String actionName = requestURI.substring(requestURI.lastIndexOf("/") + 1, requestURI.lastIndexOf(".action"));
-		return actionName;
+	//获取action
+	private Action reqToAction(HttpServletRequest req){
+		if (!req.getRequestURI().endsWith(".action")){
+			return null;
+		}
+		String path = req.getRequestURI();
+		String actionName = path.substring(path.lastIndexOf("/") + 1, path.lastIndexOf(".action"));
+		Object obj = ActionMapping.getContext().get(actionName);
+		return (Action)obj;
 	}
-
-	private Object getProxy(Action action) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+	//创建action代理实例
+	private Object createActionProxy(Action action) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
 		String classes = action.getClasses();
 		Class aClass = Class.forName(classes);
-		Object actionProxy = aClass.newInstance();
-		return actionProxy;
+		return aClass.newInstance();
+	}
+	//将属性设置到Action属性中，没考虑类型转换
+	private void setPropertyToProxy(HttpServletRequest req,Object actionProxy) throws NoSuchFieldException, IllegalAccessException {
+		Map map = req.getParameterMap();
+		for(Iterator iterator = map.keySet().iterator();iterator.hasNext();){
+			Object key = iterator.next();
+			Class aClass = actionProxy.getClass();
+			Field field = aClass.getDeclaredField(key.toString());
+			if (field == null) {
+				continue;
+			}
+			field.setAccessible(true);
+			field.set(actionProxy,req.getParameter(key.toString()));
+			field.setAccessible(false);
+		}
+	}
+	//执行方法
+	private String actionExecute(Object actionProxy,String methodName) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+		Class aClass = actionProxy.getClass();
+		Method method = aClass.getDeclaredMethod(methodName);
+		Object ret = method.invoke(actionProxy);
+		return (String)ret;
 	}
 
-	private String doAction(Action action,Object actionProxy,HttpServletRequest request) throws Exception {
-		Class aClass = actionProxy.getClass();
-		Map<String,String> params = request.getParameterMap();
-		for (String fileName : params.keySet()){
-			Field field = aClass.getDeclaredField(fileName);
-			if (field != null) {
-				field.setAccessible(true);
-				field.set(actionProxy,params.get(fileName));
-			}
-		}
-		Method method = aClass.getDeclaredMethod(action.getMethod());
-		String ret = (String)method.invoke(actionProxy);
-		return ret;
-	}
-	//只执行dispatcher和redirect类型
-	private void toLocation(Result result,Object proxy,HttpServletRequest req,HttpServletResponse resp) throws IOException, IllegalAccessException, ServletException {
+	//只处理dispatcher和redirect类型
+	private void resultExecute(Result result,Object proxy,HttpServletRequest req,HttpServletResponse resp) throws IOException, IllegalAccessException, ServletException {
 		String type = result.getType();
-		if("dispatcher".equals(type)){
-			req.getRequestDispatcher(result.getLocation()).forward(req,resp);
+		if("redirect".equals(type)){
+			resp.sendRedirect(result.getLocation());
 		}else{
-			Map<String,String> map = req.getParameterMap();
 			Field[] fields = proxy.getClass().getDeclaredFields();
 			for (Field field : fields) {
 				field.setAccessible(true);
-				map.put(field.getName(),field.get(proxy).toString());
+				req.setAttribute(field.getName(),field.get(proxy));
+				field.setAccessible(false);
 			}
-			resp.sendRedirect(result.getLocation());
+			req.getRequestDispatcher(result.getLocation()).forward(req,resp);
 		}
 
 	}
+
+
+
 }
